05966325945e1daf48170399927fd113563b193a
andresropk <afrokap@gmail.com>
2022-12-03 23:25:46 -0500

    day3 - RucksackReorganization - part 2

--- day3-RucksackReorganization.js
+++ day3-RucksackReorganization.js
@@ -312,9 +312,22 @@ ans = rucksackLists.split("\n").map(items => {
     len = items.length;
     half1 = items.slice(0,len/2)
     half2 = items.slice(len/2)
-    for(let l of half1){
+    for(let l of half1)
         if (half2.includes(l)) return priorityClue.indexOf(l);
-    }
 }).reduce((a,b)=>a+b,0);
+
+console.log(ans)
+
+
+rkskList = rucksackLists.split("\n")
+ans = 0;
+for(let i = 0; i < rkskList.length; i += 3){
+    for(let l of rkskList[i]){
+        if(rkskList[i+1].includes(l) && rkskList[i+2].includes(l)){
+            ans += priorityClue.indexOf(l);
+            break;
+        }
+    }
+}
 
 console.log(ans)
