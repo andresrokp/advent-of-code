14f5eb7143c6a7761305820e34a7e03f92a82a5a
andresropk <afrokap@gmail.com>
2022-12-06 11:24:28 -0500

    day6 - part 1

--- day6.js
+++ day6.js
@@ -1,2 +1,20 @@
 let fs = require('fs')
-let inputStr = fs.readFileSync('day6.txt','utf-8')
+let str1 = fs.readFileSync('day6.txt','utf-8')
+let str2 = 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg' // 10
+let str3 = 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw' // 11
+
+
+let str = str1;
+let span = 4
+let ans;
+for(let i = 0; i <= str.length - span; i++){
+    let chunk = str.slice(i,i+span)
+    let reps = chunk.split('').map(c => chunk.indexOf(c) === chunk.lastIndexOf(c))
+    if (reps.reduce((a,b) => a && b)){
+        ans = i + span;
+        break;
+    }
+}
+
+console.log(ans, str.slice(ans-4,ans))
+//1287
